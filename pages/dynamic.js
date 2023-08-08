4e49b670b8b27f12d758e19be380d1b971f1a48d
sohail20 <49647481+sohail20@users.noreply.github.com>
2023-08-08 15:36:13 +0500

    commit 007

--- pages/dynamic.js
+++ pages/dynamic.js
@@ -4,7 +4,7 @@ function DynamicPage({ subdomain }) {
     return (
         <div>
             hello world123 polly world testing asd1234 jjj
-            DynamicPage {subdomain} here and there
+            DynamicPage {subdomain} here and there 007
         </div>
     );
 }
