6f0d3407ac3a19e78f06365fcde2ae7aeec642f3
sohailB <bhattisohail20@gmail.com>
2023-08-08 12:04:02 +0500

    code

--- pages/home.js
+++ pages/home.js
@@ -1,23 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import CustomTextfield from "../components/CustomTextfield";
 import CustomCards from "../components/CustomCards";
+import InnerHTML from "dangerously-set-html-content";
 
 function HomePage(props) {
     const [hostName, setHostName] = useState("");
-
+    const [script, setScript] = useState(``);
     const addHostName = () => {
-        if (typeof window !== "undefined") {
-            let hostNames = localStorage.getItem("hostNames");
-            if (hostNames !== null) hostNames = JSON.parse(hostNames);
-            else hostNames = [];
-            localStorage.setItem(
-                "hostNames",
-                JSON.stringify([...hostNames, hostName.split(" ").join("")])
-            );
-            setHostName("")
-        }
+        fetch(`http://localhost:3000/api/append?url=${hostName}`,{
+            method:"POST"
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("data.script", data.script);
+                if (data.script) setScript(data.script);
+            })
+            .catch((error) => {
+                console.error("Error fetching iframe content:", error);
+            });
     };
+
+    useEffect(() => {
+        console.log("script", script);
+    }, [script]);
 
     return (
         <div mt={50}>
@@ -35,6 +41,7 @@ function HomePage(props) {
                 </Grid>
                 <Grid item xs={3}></Grid>
             </Grid>
+            {script && <p>{script}</p>}
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,5 @@ import dynamic from "next/dynamic";
 const Routers = dynamic(() => import("./routers"));
 
 export default function App(props) {
-  return <Routers wildcard={props.wildcard} />;
-}
-
-export async function getServerSideProps(context) {
-  let wildcard = context.req.headers.host.split(".")[0];
-  console.log("wildcard", wildcard)
-  if (!wildcard) {
-    wildcard = "home"; // Set a default value for the wildcard if it's not present in the request headers
-  } else {
-    wildcard =
-      wildcard !== "aussieizer"
-        ? wildcard !== "localhost:3000"
-          ? wildcard
-          : process.env.TEST_WILDCARD
-        : "home";
-  }
-
-  return { props: { wildcard: String(wildcard) } }; // Convert the wildcard to a string to ensure it's serializable
+  return <Routers />;
 }
