0cf04b20d89532c491d603490ff364e795adecc8
sohail20 <49647481+sohail20@users.noreply.github.com>
2023-08-08 15:25:31 +0500

    testing asd1234

--- pages/dynamic.js
+++ pages/dynamic.js
@@ -3,7 +3,7 @@ import React from 'react';
 function DynamicPage({ subdomain }) {
     return (
         <div>
-            hello world123 polly world
+            hello world123 polly world testing asd1234
             DynamicPage {subdomain}
         </div>
     );
