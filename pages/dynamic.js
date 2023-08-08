65be3aaa32764546889e82fa9e3ec2c6070609c3
sohail20 <49647481+sohail20@users.noreply.github.com>
2023-08-08 15:57:37 +0500

    chello

--- pages/dynamic.js
+++ pages/dynamic.js
@@ -3,7 +3,7 @@ import React from 'react';
 function DynamicPage({ subdomain }) {
     return (
         <div>
-            hello
+            hello chelo
             DynamicPage {subdomain}
         </div>
     );
