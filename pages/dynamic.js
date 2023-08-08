add3e8906efb644121cba6c4228a81a1b73adfd9
sohail20 <49647481+sohail20@users.noreply.github.com>
2023-08-08 15:33:25 +0500

    here and there

--- pages/dynamic.js
+++ pages/dynamic.js
@@ -4,7 +4,7 @@ function DynamicPage({ subdomain }) {
     return (
         <div>
             hello world123 polly world testing asd1234 jjj
-            DynamicPage {subdomain}
+            DynamicPage {subdomain} here and there
         </div>
     );
 }
